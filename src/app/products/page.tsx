80d9b8c990b9b2761888078b264848955e96ec97
Desire-e <desireeruedatorresalmeria@gmail.com>
2026-09-16 20:50:55 +0200

    fix: wrap product page in Suspense to fix a build error

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,12 +2,21 @@ import ProductsHero from '@/app/products/_components/ProductsHero'
 import TabList from "@/app/products/_components/TabList"
 import { products } from "@/app/products/_data/products";
 import { tabItems } from "@/app/products/_data/tab-items";
+import { Suspense } from "react";
 
 
 export default function Products() {
-
+    /**
+     * If a component that's part of a prerendered page tree uses useSearchParams(), 
+     * that component must be wrapped in a <Suspense> tag.
+     * This will allow any parent client components to be prerendered and sent as 
+     * part of the initial HTML.
+     */
     return (
-      <main>
+        // <Suspense> will cause the interface to pause until it's pre-rendered.
+        // In the meantime, another interface can be displayed using fallback={...}.        
+        <Suspense fallback={null}>
+        <main>
             <ProductsHero />
 
             {/* Content */}
@@ -20,8 +29,8 @@ export default function Products() {
                     />
                 </section>
             </div>  
-
-      </main>
+        </main>
+        </Suspense>
   );
 }
 
